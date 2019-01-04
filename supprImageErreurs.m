05c14151ffdc38c864431585b16f0fd5e6fe20dc
Kadir <ercinkadir90@gmail.com>
2019-01-04 16:21:27 +0100

    supprimer image inadéquates

---
+++ supprImageErreurs.m
@@ -0,0 +1,15 @@
+function supprImageErreurs(folder)
+imagefiles = dir(fullfile(folder,'*.jpg'));
+nbfail = 0;
+for i = 1:size(imagefiles,1)
+    try
+        %feature =getSommeRatios(imread(fullfile(folder,imagefiles(i).name))); CPU
+        img = imread(fullfile(folder,imagefiles(i).name));
+        imgCropped = decoupage(img);
+        partiesVisage = traitement(imgCropped);
+    catch %ME
+         nbfail = nbfail + 1;
+         delete(fullfile(folder,imagefiles(i).name));
+    end
+end
+fprintf('nb fail = %d/%d',nbfail,size(imagefiles,1));
