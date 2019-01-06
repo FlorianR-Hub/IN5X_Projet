33aff3a9ff4dad39a23cce04df76346dd80aef7f
Florian <florian.rifflart@gmail.com>
2019-01-06 19:41:18 +0100

    modifs apprentissageFCM

--- FCM/apprentissageFCM.m
+++ FCM/apprentissageFCM.m
@@ -1,7 +1,7 @@
-% Apprentissage FCM 35 37
+% Apprentissage FCM
 nbCluster = 6;
 
-m = 2; %1.7
+m = 2;
 epsillon = 1e-05;
 maxIter = 100;
 options = [m maxIter epsillon true];
@@ -9,22 +9,14 @@ options = [m maxIter epsillon true];
 features = [];
 for trancheAge = 30 : 10 : 70
     folder = fullfile('database', num2str(trancheAge));
-    features = [features ; getAllFeatures(folder)];
+    features = [features ; getAllFeatures(folder)]; %#ok<*AGROW>
 end
-csvwrite('apprentissage\all_tranches_age.csv', features);
+%csvwrite('apprentissage\all_tranches_age.csv', features);
 
 Age = features(:,1);
 Ratios = features(:,2);
 
 [Centres, ~] = fcm(features,nbCluster,options);
-
-% Calcul de l'age moyen de chaque cluster
-% M stocke le nombre de donnés (ratios) aggloméres par un cluster, 
-% ainsi que la somme des ages des données aggloméres, afin de calculer
-% un age moyen pour chaque cluster
-M = zeros(nbCluster,2);
-
-
 csvwrite('apprentissage\FCM.csv', Centres);
 
 display(strcat('Apprentissage FCM terminé !'));
