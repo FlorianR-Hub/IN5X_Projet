34b368bb77f893b7ab01a679994e3eaa9a031df6
Florian <florian.rifflart@gmail.com>
2019-01-06 23:26:27 +0100

    nettoyage decisionFCM

--- FCM/decisionFCM.m
+++ FCM/decisionFCM.m
@@ -1,23 +1,22 @@
-% Decision de l'age d'une image
-% On calcule le degrï¿½ d'appartenance Pi du ratio aux clusters
-% age = somme (Pi * age_cluster_i) / somme (Pi)
+% Décision de l'age d'une image
+% On calcule le degré d'appartenance Pi du ratio aux clusters
+% age = somme(Pi * age_cluster_i)
 
 function age = decisionFCM(classifieur, img, m)
-    feature = getSommeRatios(img);
-    sumCoeff = 0;
+    sommeRatios = getSommeRatios(img);
     age = 0;
     exp = 1; % exp pour faire plus ou moins influer les coefficient des centres les plus proches
-    for i = 1 : size(classifieur,1)
-            if( isnan(classifieur(i,1)) == false)
-                dist = feature - classifieur(i,2);
-                somme = 0;
-                for j = 1 : size(classifieur,1)
-                    somme = somme +(dist / (feature - classifieur(j,2)))^(2 / (m-1));
-                end
-                % 1/somme = degrï¿½ d'appartenance de l'image au cluster i
-                sumCoeff = sumCoeff + (1 / somme^exp);
-                age = age + (1 / somme^exp) * classifieur(i,1); 
+    
+    for j = 1 : size(classifieur,1)
+        if(isnan(classifieur(j,1)) == false)
+            dist = sommeRatios - classifieur(j,2);
+            somme = 0;
+            for k = 1 : size(classifieur,1)
+                somme = somme + (dist / (sommeRatios - classifieur(k,2)))^(2 / (m-1));
             end
+            % degré d'appartenance Pi de l'image au cluster j
+            Pi = 1 / somme^exp;
+            age = age + Pi * classifieur(j,1); 
+        end
     end
-    age = age / (sumCoeff);
 end
